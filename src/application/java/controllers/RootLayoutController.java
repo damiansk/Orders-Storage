7d16423e31b327d8118bd9beb655e9fa2421c422
bumpMind <damian.s.ja@gmail.com>
2017-02-15 11:04:19 +0100

    Added RootLayout view with connected controller

---
+++ src/application/java/controllers/RootLayoutController.java
@@ -0,0 +1,26 @@
+package application.java.controllers;
+
+import javafx.fxml.FXML;
+
+public class RootLayoutController {
+
+	@FXML
+	private void initialize() {
+	}
+
+	@FXML
+	private void deleteChosenOrder() {
+	}
+	
+	@FXML
+	private void addNewOrder() {
+	}
+	
+	@FXML 
+	private void showProviderView() {
+	}
+	
+	@FXML
+	private void closeApplication() {
+	}
+}
