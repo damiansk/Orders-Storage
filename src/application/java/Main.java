5b3564526ec2033959f64bd03cb4a63b25d6f9a0
bumpMind <damian.s.ja@gmail.com>
2017-02-15 11:05:18 +0100

    Added initView to main

--- src/application/java/Main.java
+++ src/application/java/Main.java
@@ -2,6 +2,9 @@ package application.java;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +15,20 @@ public class Main extends Application {
 
     @Override
     public void start( Stage primaryStage ) throws Exception {
+        initView( primaryStage );
+    }
 
+    private void initView( Stage primaryStage ) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+
+        loader.setLocation( Main.class.getResource("../resources/view/RootLayout.fxml") );
+
+        BorderPane layout = loader.load();
+
+        Scene scene = new Scene(layout);
+
+        primaryStage.setTitle("");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
