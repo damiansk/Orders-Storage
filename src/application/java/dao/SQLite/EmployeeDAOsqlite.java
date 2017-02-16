18b25f2e7e053949b5b00e9ca9e7ce7258de055b
bumpMind <damian.s.ja@gmail.com>
2017-02-16 18:02:54 +0100

    Added EmployeeDAOsqlite

---
+++ src/application/java/dao/SQLite/EmployeeDAOsqlite.java
@@ -0,0 +1,35 @@
+package application.java.dao.SQLite;
+
+import application.java.dao.EmployeeDAO;
+import application.java.dto.EmployeeDTO;
+
+import java.util.List;
+
+public class EmployeeDAOsqlite implements EmployeeDAO {
+
+    @Override
+    public EmployeeDTO getEmployee(int id) {
+        return null;
+    }
+
+    @Override
+    public List<EmployeeDTO> getAllEmployee() {
+        return null;
+    }
+
+    @Override
+    public void insertEmployee(EmployeeDTO employee) {
+
+    }
+
+    @Override
+    public void updateEmployee(EmployeeDTO employee) {
+
+    }
+
+    @Override
+    public void deleteEmployee(EmployeeDTO employee) {
+
+    }
+
+}
