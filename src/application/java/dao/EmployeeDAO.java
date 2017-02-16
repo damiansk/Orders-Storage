a19f8b78bde366b5eb88885cbe68af7d243e2ac6
bumpMind <damian.s.ja@gmail.com>
2017-02-16 18:02:40 +0100

    Added getAllEmployee method

--- src/application/java/dao/EmployeeDAO.java
+++ src/application/java/dao/EmployeeDAO.java
@@ -4,10 +4,14 @@ package application.java.dao;
 import application.java.dto.EmployeeDTO;
 import com.sun.istack.internal.Nullable;
 
+import java.util.List;
+
 public interface EmployeeDAO {
 
     @Nullable
     public EmployeeDTO getEmployee(int id);
+
+    public List<EmployeeDTO> getAllEmployee();
 
     public void insertEmployee(EmployeeDTO employee);
 
